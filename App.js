b87acee104d753e2b9d06f6e27c4f7cedfaf1885
Pablo Gastón Hiyano <pablo.hiyano@globant.com>
2016-02-15 17:22:15 -0300

    video 6

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ class App extends React.Component {
 	constructor(){
 		super();
 		this.state = { txt: 'this is the state text'}
+		this.update = this.update.bind(this)
 	}
 
 	update(e){
@@ -13,12 +14,25 @@ class App extends React.Component {
 	render() {
 		return ( 
 			<div>
-				<input type="text"
-					onChange={this.update.bind(this)} />
-				<h1>{this.state.txt}</h1>
+				<Widget txt={this.state.txt} update={this.update} />
+				<Widget txt={this.state.txt} update={this.update} />
+				<Widget txt={this.state.txt} update={this.update} />
+				<Widget txt={this.state.txt} update={this.update} />
+				<Widget txt={this.state.txt} update={this.update} />
 			</div>
 		);
 	}
+}
+
+
+const Widget = (props) => {
+	return ( 
+		<div>
+			<input type="text"
+				onChange={props.update} />
+			<h1>{props.txt}</h1>
+		</div>
+	);
 }
 
 export default App
